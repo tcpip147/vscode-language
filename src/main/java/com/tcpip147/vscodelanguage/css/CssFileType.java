196d21a662636215c8d90ed2926a844bff6839ae
tcpip147 <tcpip147@gmail.com>
2024-06-02 11:29:48 +0900

    4

--- src/main/java/com/tcpip147/vscodelanguage/css/CssFileType.java
+++ src/main/java/com/tcpip147/vscodelanguage/css/CssFileType.java
@@ -12,7 +12,7 @@ import javax.swing.*;
 public class CssFileType extends LanguageFileType {
 
     public static final CssFileType INSTANCE = new CssFileType();
-    public static final Icon ICON = IconLoader.getIcon("/css.png", CssFileType.class);
+    public static final Icon ICON = IconLoader.getIcon("/css.svg", CssFileType.class);
     public static final String EXTENSION = "css";
 
     protected CssFileType() {
